e58b43849210cbea392073e59f82699b2c5bf5f6
Atsushi-cook <sacook7@icloud.com>
2020-01-25 17:28:32 +0900

    incremental 1th commit

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,40 +2,40 @@ $(function(){
   function buildHTML(message){
    if ( message.image ) {
      var html =
-      `<div class="main-chat" data-message-id=${message.id}>
-         <div class="chat-main__group-info">
-           <div class="chat-main__group-one">
-             ${message.user_name}
-           </div>
-           <div class="chat-main__group-two">
-             ${message.created_at}
-           </div>
-         </div>
-         <div class="chat-main__message-send">
-           <p class="chat-main__message-send__content">
-             ${message.content}
-           </p>
-         </div>
-         <img src=${message.image} >
-       </div>`
-     return html;
-   } else {
-     var html =
-      `<div class="main-chat" data-message-id=${message.id}>
-         <div class="chat-main__group-info">
-           <div class="chat-main__group-one">
-             ${message.user_name}
-           </div>
-           <div class="chat-main__group-two">
-             ${message.created_at}
-           </div>
-         </div>
-         <div class="chat-main__message-send">
-           <p class="chat-main__message-send__content">
-             ${message.content}
-           </p>
-         </div>
-       </div>`
+          `<div class="message" data-message-id=${message.id}>
+          <div class="upper-message">
+            <div class="upper-message__user-name">
+              ${message.user_name}
+            </div>
+            <div class="upper-message__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="lower-message">
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
+          </div>
+          <img src=${message.image} >
+        </div>`
+      return html;
+      } else {
+      var html =
+        `<div class="message" data-message-id=${message.id}>
+          <div class="upper-message">
+            <div class="upper-message__user-name">
+              ${message.user_name}
+            </div>
+            <div class="upper-message__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="lower-message">
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
+          </div>
+        </div>`
      return html;
    };
  }
@@ -52,7 +52,9 @@ $('#new_message').on('submit', function(e){
    contentType: false
  })
   .done(function(data){
+    console.log(data);
     var html = buildHTML(data);
+    
     $('.main-chat__second').append(html);
     $('.main-chat__second').animate({scrollTop: $('.main-chat__second')[0].scrollHeight},);
     $('form')[0].reset();
